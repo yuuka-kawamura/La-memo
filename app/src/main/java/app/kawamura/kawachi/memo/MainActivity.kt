a3d8d85f09b4034bb27f9f023231d2ee9db76c16
yuuka-kawamura <yuuka.yokohama@gmail.com>
2022-09-05 15:54:43 +0900

    リスト表示完成

--- app/src/main/java/app/kawamura/kawachi/memo/MainActivity.kt
+++ app/src/main/java/app/kawamura/kawachi/memo/MainActivity.kt
@@ -1,15 +1,43 @@
 package app.kawamura.kawachi.memo
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import app.kawamura.kawachi.memo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val pref:SharedPreferences=getSharedPreferences("SharedPref", Context.MODE_PRIVATE)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
-    var data=
+
+        var data = arrayListOf(
+            "a", "i"
+        )
+
+        val list = findViewById<ListView>(R.id.list_view)
+        list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+        var addtext = intent.getStringExtra("Text")
+        if(addtext!==null)
+        data.add(addtext.toString())
+
+        binding.plusButton.setOnClickListener {
+            val toMemoActivity = Intent(this, MemoActivity::class.java)
+            startActivity(toMemoActivity)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        val editor=pref.edit()
+       // editor.putStringSet("Key",data)
+
     }
 }
--- app/src/main/java/app/kawamura/kawachi/memo/MemoActivity.kt
+++ app/src/main/java/app/kawamura/kawachi/memo/MemoActivity.kt
@@ -1,14 +1,26 @@
 package app.kawamura.kawachi.memo
 
+import android.content.Intent
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import app.kawamura.kawachi.memo.databinding.ActivityMemoBinding
 
 class MemoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMemoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMemoBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+        binding = ActivityMemoBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        var editText=binding.memoEdittext.text.toString()
+
+        binding.checkButton.setOnClickListener {
+
+            val toMainActivity = Intent(this, MainActivity::class.java)
+            toMainActivity.putExtra("Text", editText)
+
+            startActivity(toMainActivity)
+        }
     }
 }
