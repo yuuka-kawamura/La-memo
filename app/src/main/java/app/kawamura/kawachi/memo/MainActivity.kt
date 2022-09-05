e2fde13003e762ab41e3bea9f2e384ed7ecb3ec6
yuuka-kawamura <yuuka.yokohama@gmail.com>
2022-09-05 16:32:52 +0900

    SharedPreferencesエラー

--- app/src/main/java/app/kawamura/kawachi/memo/MainActivity.kt
+++ app/src/main/java/app/kawamura/kawachi/memo/MainActivity.kt
@@ -8,25 +8,32 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import app.kawamura.kawachi.memo.databinding.ActivityMainBinding
+import java.lang.reflect.Array.set
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val pref:SharedPreferences=getSharedPreferences("SharedPref", Context.MODE_PRIVATE)
+    private val pref: SharedPreferences = getSharedPreferences("SharedPref", Context.MODE_PRIVATE)
+
+    //private lateinit var data:list
+    var data = listOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
 
-        var data = arrayListOf(
+        data = arrayListOf(
             "a", "i"
         )
 
+       memodata=pref.getStringSet("Key",null)
+        if(memodata!==null)
+            data.add(memodata)
         val list = findViewById<ListView>(R.id.list_view)
         list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
         var addtext = intent.getStringExtra("Text")
-        if(addtext!==null)
-        data.add(addtext.toString())
+        if (addtext !== null)
+            data.add(addtext.toString())
 
         binding.plusButton.setOnClickListener {
             val toMemoActivity = Intent(this, MemoActivity::class.java)
@@ -36,8 +43,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
-        val editor=pref.edit()
-       // editor.putStringSet("Key",data)
-
+        val editor = pref.edit()
+        editor.putStringSet("Key", data)
+        editor.apply()
     }
 }
