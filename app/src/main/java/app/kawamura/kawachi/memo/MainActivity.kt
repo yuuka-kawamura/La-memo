b37fec2142e6c1b7410dbaeae81509afbd00af8e
yuuka-kawamura <yuuka.yokohama@gmail.com>
2022-09-06 16:36:54 +0900

    SharedPreferences アプリが落ちる

--- app/src/main/java/app/kawamura/kawachi/memo/MainActivity.kt
+++ app/src/main/java/app/kawamura/kawachi/memo/MainActivity.kt
@@ -3,19 +3,22 @@ package app.kawamura.kawachi.memo
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.TypedArrayUtils.getString
 import app.kawamura.kawachi.memo.databinding.ActivityMainBinding
-import java.lang.reflect.Array.set
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val pref: SharedPreferences = getSharedPreferences("SharedPref", Context.MODE_PRIVATE)
 
-    //private lateinit var data:list
-    var data = listOf<String>()
+    val gson = Gson()
+    var data = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +29,17 @@ class MainActivity : AppCompatActivity() {
             "a", "i"
         )
 
-       memodata=pref.getStringSet("Key",null)
-        if(memodata!==null)
-            data.add(memodata)
         val list = findViewById<ListView>(R.id.list_view)
         list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
         var addtext = intent.getStringExtra("Text")
         if (addtext !== null)
             data.add(addtext.toString())
+
+        data = gson.fromJson(
+            pref.getString("Key", ""),
+            object : TypeToken<List<String?>?>() {}.type
+        )
+
 
         binding.plusButton.setOnClickListener {
             val toMemoActivity = Intent(this, MemoActivity::class.java)
@@ -44,7 +50,7 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         val editor = pref.edit()
-        editor.putStringSet("Key", data)
+        editor.putString("Key", gson.toJson(data))
         editor.apply()
     }
 }
